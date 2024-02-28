5d7bba42d938872883c7fc0a83b391653193b6af
Kimia <kimia_afshari@ucsb.edu>
2024-02-27 22:41:44 -0800

    updated generation script

--- generate.sh
+++ generate.sh
@@ -1,7 +1,9 @@
- python scripts/stable_txt2img.py \
- --ddim_eta 0.0  --n_samples 8   \
- --n_iter 1  --scale 10.0  --ddim_steps 100 \
- --ckpt "logs/dog2024-02-26T23-52-37_dog training/checkpoints/last.ckpt"  \
- --prompt "turn it into a cat sitting near the ocean" \
- --outdir "logs/dog2024-02-26T23-52-37_dog training/outputs"  \
- --config configs/generate.yaml \
+root="logs/training2024-02-27T15-56-50_dog training"
+python scripts/stable_txt2img.py \
+--ddim_eta 0.0 --n_samples 8 \
+--n_iter 1 --scale 10.0 --ddim_steps 100 \
+--ckpt "${root}/checkpoints/last.ckpt" \
+--prompt "replace the horse with a sks dog" \
+--outdir "${root}/outputs" \
+--config configs/generate.yaml
+
