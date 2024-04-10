e19d50b05383b251fa9785ec5b10ea48d5cbd0a4
rastadayon <rastadayon@gmail.com>
2024-04-09 17:18:08 -0700

    Modify .gitignore and train.sh

--- train.sh
+++ train.sh
@@ -1,11 +1,10 @@
 ulimit -n 4096
 
-python main.py --base configs/stable-diffusion/v1-finetune_unfrozen.yaml  -t \
-#  --actual_resume models/stable-diffusion/sd-v1-4-full-ema.ckpt  \
+python main.py --base configs/stable-diffusion/v1-finetune_unfrozen.yaml -t \
   --actual_resume models/instruct-pix2pix/instruct-pix2pix-00-22000.ckpt \
-  -n "dog training"  \
+  -n "wolf_to_dog"  \
   --gpus 0,1,2,3,4  \
-  --data_root data/training/dog/  \
-  --reg_data_root data/regularization/ \
+  --data_root data/golden_dog/training  \
+  --reg_data_root data/golden_dog/regularization/ \
   --class_word dog \
   --no-test
